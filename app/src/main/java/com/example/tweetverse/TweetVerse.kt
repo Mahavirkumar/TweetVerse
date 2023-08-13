7411055144ef9feb2efd845d8682de48a3cc03c4
mahavir <mahavirkumarbabu@gmail.com>
2023-08-13 12:09:19 +0530

    init commit,project setup done

---
+++ app/src/main/java/com/example/tweetverse/TweetVerse.kt
@@ -0,0 +1,4 @@
+package com.example.tweetverse
+
+class TweetVerse {
+}
---
+++ app/src/main/java/com/example/tweetverse/models/TweetListItem.kt
@@ -0,0 +1,6 @@
+package com.example.tweetverse.models
+
+data class TweetListItem(
+    val category: String,
+    val text: String
+)
